15336b7a861e38adf2d1fcb272c0ebc18424010a
Vijay <vijay.nagaraj2007@gmail.com>
2019-09-04 22:26:12 +0530

    First commit.

---
+++ src/HandlingPopUps/AutoITDemo.java
@@ -0,0 +1,12 @@
+package HandlingPopUps;
+
+import java.io.IOException;
+
+public class AutoITDemo 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		Runtime.getRuntime().exec("C:\\Users\\Vijay\\Documents\\Testing Course\\Automation\\calc_demo.exe");
+	}
+
+}
